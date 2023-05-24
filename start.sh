c36eb99fa50fbeb1cc7e05e451976e36f870dc1c
jay <aisenbreyjay@gmail.com>
2023-05-23 23:13:17 -0500

    Move to a single line in start.sh though I'm not sure it matters

--- start.sh
+++ start.sh
@@ -1,2 +1,1 @@
-sudo update-alternatives --set java /usr/lib/jvm/java-17-openjdk-amd64/bin/java &&
-sudo ./gradlew bootrun --args='--spring.profiles.active=production' > wahoo.log 2>&1 &
+sudo update-alternatives --set java /usr/lib/jvm/java-17-openjdk-amd64/bin/java && sudo ./gradlew bootrun --args='--spring.profiles.active=production' > ~/logs/gateway.log 2>&1 &
